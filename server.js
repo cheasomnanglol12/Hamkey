d91859525b5f657cba52caa7cca62029e68aa687
cheasomnanglol12 <mybrolol096228@gmail.com>
2024-08-09 00:21:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
+const hamkey = require('./hamkey');
 
 app.use(express.json());
 
-const games = require('./games'); // Load the games script
-
 app.post('/generate/:game', async (req, res) => {
   const game = req.params.game;
-  const code = await games.displayPromoCode(game);
-  res.send(code);
+  try {
+    const code = await hamkey.displayPromoCode(game);
+    res.send(code);
+  } catch (err) {
+    res.status(500).send(`Error generating promo code: ${err.message}`);
+  }
 });
 
 app.listen(3000, () => {
