966799444e2ec1f6e97cde6cdfb52d518b2456aa
cheasomnanglol12 <mybrolol096228@gmail.com>
2024-08-09 00:17:32 +0700

    Create games.js

---
+++ games.js
@@ -0,0 +1,8 @@
+// ... (rest of the script remains the same)
+
+async function displayPromoCode(gameKey) {
+  // ... (rest of the function remains the same)
+  return promoCode;
+}
+
+module.exports = { displayPromoCode };
