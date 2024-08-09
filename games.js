71bfb7aac860f759dbb43e2b1f4e7ccd585350dc
cheasomnanglol12 <mybrolol096228@gmail.com>
2024-08-09 12:37:14 +0700

    Delete games.js

--- games.js
+++
@@ -1,8 +0,0 @@
-// ... (rest of the script remains the same)
-
-async function displayPromoCode(gameKey) {
-  // ... (rest of the function remains the same)
-  return promoCode;
-}
-
-module.exports = { displayPromoCode };
